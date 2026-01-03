9e7495b5d7314fe3e7562f5aeb773098ece99197
dylanncairns <dylancccairns@gmail.com>
2026-01-03 14:07:25 -0800

    Initialize new SwiftUI client, ignore prototype and move it to archive

---
+++ ios/VitalClient/VitalClient/VitalClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  VitalClientApp.swift
+//  VitalClient
+//
+//  Created by Dylan Cairns on 1/3/26.
+//
+
+import SwiftUI
+
+@main
+struct VitalClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
